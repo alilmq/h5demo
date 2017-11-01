310aac136e1c5174a66f4fa0998239d7b91dc1aa
alilmq <alilmq@163.com>
2017-11-01 10:18:15 +0800

    使用2.2.0版本

--- webpack.config.js
+++ webpack.config.js
@@ -90,7 +90,7 @@ module.exports = {
     },{
       name: 'prismplayer',
       var: 'prismplayer',
-      url: 'https://g.alicdn.com/de/prismplayer/2.1.0/aliplayer-min.js'
+      url: 'https://g.alicdn.com/de/prismplayer/2.2.0/aliplayer-min.js'
     },{
       name: 'mqttws31',
       var: 'mqttws31',
