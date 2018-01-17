0b33efd2548f90615dfa992c4d52114b8a372cb0
alilmq <alilmq@163.com>
2018-01-17 12:11:46 +0800

    评论优化，输入评论后滚动条滚到最下面

--- src/js/comment/index.js
+++ src/js/comment/index.js
@@ -4,11 +4,11 @@ require('./index.css');
 export default class CommentBuffer {
 	constructor(wrapper, player) {
 		this.comments = [];
+		this._wrapper = wrapper;
 		this.liveComment = new VideoComments(wrapper,player);
 		let that = this;
 		Event.on(Event.EventConstant.Comments_Sended, (e,data)=>{
 			this.add(data);
-			wrapper.scrollTop(wrapper[0].scrollHeight);
 		})
 	}
 
@@ -52,6 +52,7 @@ export default class CommentBuffer {
 				}
 				let comment = that.comments.shift();
 				that.liveComment.send(comment);
+				that._wrapper.scrollTop(that._wrapper[0].scrollHeight);
 
 			} else {
 				that.isWorking = false;
